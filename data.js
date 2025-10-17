0b60d8c27ea603dd513339dd352ac6e117ab7a85
BENJAMIN7816 <benjaminsebastian7816@gmail.com>
2025-10-17 11:42:53 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hall-jpg",
+      "name": "hall jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1257400355760865,
+          "pitch": 0.286963400573347,
+          "rotation": 0,
+          "target": "1-liviong-jpg"
+        },
+        {
+          "yaw": 0.26249062785936417,
+          "pitch": 0.09620527352984709,
+          "rotation": 0,
+          "target": "2-wash-jpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-liviong-jpg",
+      "name": "liviong jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "yaw": 0.2901834781402677,
+        "pitch": -0.007914562444314654,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6929467936729861,
+          "pitch": 0.36740371390328974,
+          "rotation": 0,
+          "target": "0-hall-jpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-wash-jpg",
+      "name": "wash jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2980198987298799,
+          "pitch": 0.1691980279938381,
+          "rotation": 0,
+          "target": "0-hall-jpg"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
